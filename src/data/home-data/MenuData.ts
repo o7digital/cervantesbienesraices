68e96e910670fd92553a24ca9c603e650bb4d37f
o7digital <101292703+o7digital@users.noreply.github.com>
2025-08-21 18:36:12 -0600

    Initialisation du projet Cervantes Bienes Raíces basé sur LC Inmobiliaria

---
+++ src/data/home-data/MenuData.ts
@@ -0,0 +1,54 @@
+interface MenuItem {
+  id: number;
+  title: string;
+  class_name?: string;
+  link: string;
+  has_dropdown: boolean;
+  sub_menus?: {
+    link: string;
+    title: string;
+  }[];
+  menu_column?: {
+    id: number;
+    mega_title: string;
+    mega_menus: {
+      link: string;
+      title: string;
+    }[];
+  }[];
+}
+
+const menu_data: MenuItem[] = [
+  {
+    id: 1,
+    title: "Home",
+    link: "/",
+    has_dropdown: false,
+  },
+  {
+    id: 2,
+    title: "Quiénes Somos",
+    link: "/quienes-somos",
+    has_dropdown: false,
+  },
+  {
+    id: 3,
+    title: "Servicios",
+    link: "/servicios",
+    has_dropdown: false,
+  },
+  {
+    id: 4,
+    title: "Consultoría",
+    link: "/consultoria",
+    has_dropdown: false,
+  },
+  {
+    id: 5,
+    title: "Contacto",
+    link: "/contacto",
+    has_dropdown: false,
+  },
+];
+
+export default menu_data;
