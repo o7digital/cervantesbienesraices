7a2f6077a4c2b9ac0228537f6154cf1c98565d86
o7digital <101292703+o7digital@users.noreply.github.com>
2025-09-01 22:21:25 -0600

    Update: Hero con botón grande 'Ver todas las propiedades' usando EasyBroker

--- src/components/homes/home-eight/Hero.tsx
+++ src/components/homes/home-eight/Hero.tsx
@@ -1,8 +1,6 @@
 "use client"
 import Image from "next/image"
 
-import icon_1 from "@/assets/images/icon/icon_75.svg"
-
 const Hero = () => {
    return (
       <div className="hero-banner-eight z-1 pt-250 xl-pt-200 pb-250 xl-pb-150 lg-pb-100 position-relative">
@@ -11,7 +9,7 @@ const Hero = () => {
                <div className="col-xl-9 col-lg-10 col-md-10 m-auto">
                   {/* ✅ Título cambiado */}
                   <h1 className="hero-heading text-white text-center wow fadeInUp">
-                     CERVANTES BIENES RAICES
+                     CERVANTES BIENES RAÍCES
                   </h1>
                   {/* ✅ Subtítulo cambiado */}
                   <p 
@@ -22,29 +20,17 @@ const Hero = () => {
                   </p>
                </div>
             </div>
-            <div className="search-wrapper-four me-auto ms-auto mt-45 lg-mt-20 position-relative">
-               <nav className="d-flex justify-content-center">
-                  <div className="nav nav-tabs border-0" role="tablist">
-                     <button className="nav-link active" id="buy-tab" data-bs-toggle="tab" data-bs-target="#buy" type="button" role="tab" aria-controls="buy" aria-selected="true">Rent</button>
-                     <button className="nav-link" id="rent-tab" data-bs-toggle="tab" data-bs-target="#rent" type="button" role="tab" aria-controls="rent" aria-selected="false">Sale</button>
-                  </div>
-               </nav>
-               <div className="bg-wrapper mt-30">
-                  <div className="tab-content">
-                     <div className="tab-pane fade show active" id="buy" role="tabpanel" aria-labelledby="buy-tab" tabIndex={0}>
-                        <form onSubmit={(e) => e.preventDefault()} className="position-relative z-1">
-                           <input type="text" placeholder="Type location name here..." />
-                           <button className="tran3s"><Image src="" alt="" className="m-auto" /></button>
-                        </form>
-                     </div>
-                     <div className="tab-pane fade" id="rent" role="tabpanel" aria-labelledby="rent-tab" tabIndex={0}>
-                        <form onSubmit={(e) => e.preventDefault()} className="position-relative z-1">
-                           <input type="text" placeholder="Type location name here..." />
-                           <button className="tran3s"><Image src={icon_1} alt="" className="m-auto" /></button>
-                        </form>
-                     </div>
-                  </div>
-               </div>
+
+            {/* ✅ Botón EasyBroker en lugar del buscador */}
+            <div className="d-flex justify-content-center mt-45 lg-mt-20">
+               <a 
+                  href="https://cervantesbienesraices.easybroker.com/properties" 
+                  target="_blank" 
+                  rel="noopener noreferrer" 
+                  className="btn btn-lg btn-light fw-bold px-5 py-3"
+               >
+                  Ver todas las propiedades
+               </a>
             </div>
          </div>
       </div>
