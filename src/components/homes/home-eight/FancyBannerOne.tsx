765fea31a958bfa007d56de1050e4fe00c0043cb
o7digital <101292703+o7digital@users.noreply.github.com>
2025-10-10 17:48:30 -0600

    Show Javier Cervantes photo in home banner

--- src/components/homes/home-eight/FancyBannerOne.tsx
+++ src/components/homes/home-eight/FancyBannerOne.tsx
@@ -1,53 +1,62 @@
+import Image from "next/image";
+
 const FancyBannerOne = () => {
-   return (
-      <div className="fancy-banner-nine mt-110 lg-mt-80">
-         <div className="container container-large">
-            <div className="row align-items-center">
-               <div className="col-lg-4">
-                  <div className="d-flex align-items-center md-mb-30">
-                     <div className="avatar placeholder rounded-circle d-flex align-items-center justify-content-center">
-                        <i className="fa-regular fa-user fs-24"></i>
-                     </div>
-                     <div className="ps-3 text">
-                        <h6 className="fs-22">Javier Cervantes</h6>
-                        <span className="fs-20">Fundador y CEO de CERVANTES BIENES RAICES</span>
-                     </div>
-                  </div>
-               </div>
-               <div className="col-xxl-7 col-lg-8">
-                  <blockquote>&quot;Seguimos un proceso para ofrecer a nuestros inversionistas las mejores oportunidades.&quot;</blockquote>
-               </div>
+  return (
+    <div className="fancy-banner-nine mt-110 lg-mt-80">
+      <div className="container container-large">
+        <div className="row align-items-center">
+          <div className="col-lg-4">
+            <div className="d-flex align-items-center md-mb-30">
+              <div className="avatar rounded-circle overflow-hidden position-relative">
+                <Image
+                  src="/assets/images/javier-cervantes.jpeg"
+                  alt="Javier Cervantes"
+                  fill
+                  sizes="60px"
+                  style={{ objectFit: "cover" }}
+                  priority
+                />
+              </div>
+              <div className="ps-3 text">
+                <h6 className="fs-22">Javier Cervantes</h6>
+                <span className="fs-20">Fundador y CEO de CERVANTES BIENES RAICES</span>
+              </div>
             </div>
+          </div>
+          <div className="col-xxl-7 col-lg-8">
+            <blockquote>&quot;Seguimos un proceso para ofrecer a nuestros inversionistas las mejores oportunidades.&quot;</blockquote>
+          </div>
+        </div>
 
-            <div className="fact-wrapper d-flex flex-wrap mt-60 md-mt-40">
-               <div className="fact-box">
-                  <div className="text-meta text-sm-start d-inline-block">
-                     <div className="numb">300</div>
-                     <span>Terrenos Analizados</span>
-                  </div>
-               </div>
-               <div className="fact-box">
-                  <div className="text-meta text-sm-start d-inline-block">
-                     <div className="numb">0.2%</div>
-                     <span>Pasaron la evaluacion inicial</span>
-                  </div>
-               </div>
-               <div className="fact-box">
-                  <div className="text-meta text-sm-start d-inline-block">
-                     <div className="numb">15%</div>
-                     <span>Aprobados por el Comite</span>
-                  </div>
-               </div>
-               <div className="fact-box">
-                  <div className="text-meta text-sm-start d-inline-block">
-                     <div className="numb">5%</div>
-                     <span>Adquiridos</span>
-                  </div>
-               </div>
+        <div className="fact-wrapper d-flex flex-wrap mt-60 md-mt-40">
+          <div className="fact-box">
+            <div className="text-meta text-sm-start d-inline-block">
+              <div className="numb">300</div>
+              <span>Terrenos Analizados</span>
             </div>
-         </div>
+          </div>
+          <div className="fact-box">
+            <div className="text-meta text-sm-start d-inline-block">
+              <div className="numb">0.2%</div>
+              <span>Pasaron la evaluacion inicial</span>
+            </div>
+          </div>
+          <div className="fact-box">
+            <div className="text-meta text-sm-start d-inline-block">
+              <div className="numb">15%</div>
+              <span>Aprobados por el Comite</span>
+            </div>
+          </div>
+          <div className="fact-box">
+            <div className="text-meta text-sm-start d-inline-block">
+              <div className="numb">5%</div>
+              <span>Adquiridos</span>
+            </div>
+          </div>
+        </div>
       </div>
-   )
-}
+    </div>
+  );
+};
 
-export default FancyBannerOne
+export default FancyBannerOne;
