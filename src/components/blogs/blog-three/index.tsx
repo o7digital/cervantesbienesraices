3dd711cfd2c2872b755523adb506e7d85ccefaac
o7digital <101292703+o7digital@users.noreply.github.com>
2026-01-19 10:51:04 -0600

    Use photo hero for blog breadcrumb

--- src/components/blogs/blog-three/index.tsx
+++ src/components/blogs/blog-three/index.tsx
@@ -4,6 +4,7 @@ import FooterFour from "@/layouts/footers/FooterFour";
 import HeaderOne from "@/layouts/headers/HeaderOne";
 import { BlogPost } from "@/types/blog";
 import BlogThreeArea from "./BlogThreeArea";
+import heroBlog from "@/assets/images/blog/blog_img_03.jpg";
 
 type Props = {
   posts: BlogPost[];
@@ -19,6 +20,7 @@ const BlogThree = ({ posts }: Props) => {
         link_title="Inicio"
         sub_title="Artículos"
         style={true}
+        bgImage={heroBlog.src}
       />
       <BlogThreeArea posts={posts} />
       <FancyBanner />
